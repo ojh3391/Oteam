d72520f0b8efd2e3aa7a76a2731c12292cba56e1
gograzio <grazio@gmail.com>
2018-01-03 14:39:05 +0900

    sql 변경

--- CFK/cfk.sql
+++ CFK/cfk.sql
@@ -8,21 +8,24 @@ create table cfk_user(
 	user_name varchar(12) not null,
 	user_email varchar(30) not null,
 	user_gender char(1) not null,
+	user_attend_date varchar(10),
 	user_check_vote int
 );
 
+drop table cfk_user;
+
 create table cfk_board(
-	board_num int not null,
+	board_num int primary key,
 	board_subject varchar(50) not null,
 	board_content varchar(2000) not null,
 	board_file varchar(50),
 	board_real_file varchar(50),
 	board_thumnail varchar(50),
-	board_writer varchar(20) not null,
+	board_writer varchar(20),
 	board_vote int default 0,
-	board_readcount int default 0;
-	board_date date,	
-	primary key (board_num)
+	board_readcount int default 0,
+	board_date date	
+	
 );	
 
 create table cfk_reply(
